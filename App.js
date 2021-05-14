82189e0c677a8182985bf6a430f4b32cf5468603
Daniel-Nice-Marsh <60605432+Daniel-Nice-Marsh@users.noreply.github.com>
2021-05-14 18:07:29 +0100

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render(){
+    return (
+      //view changed to <Container> & <text> changed to <Title>
+      <Container>
+        <Title>Hello, World!</Title>
+      </Container>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+  flex: 1;
+  background-color: #eee;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Title = styled.Text`
+  font-size: 25px;
+  font-weight: 500;
+  color: #db7093;
+`;
+
