9410a4ad8ce7560a19e315d7369c926d9a009866
Daniel-Nice-Marsh <60605432+Daniel-Nice-Marsh@users.noreply.github.com>
2021-05-17 19:21:25 +0100

    diary page first implementation

--- App.js
+++ App.js
@@ -2,6 +2,7 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';  
+import Task from './components/Task';
 
 import { NavigationContainer } from '@react-navigation/native';  
 import { createStackNavigator } from '@react-navigation/stack';
--- screens/DiaryScreen.js
+++ screens/DiaryScreen.js
@@ -1,12 +1,104 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, View } from 'react-native';
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import Task from '../components/Task';
 
 function DiaryScreen(props) {
+    const [task, setTask] = useState(); // create state.
+    const [taskItems, setTaskItems] = useState([]);
+
+    const handleAddTask = () => {
+        Keyboard.dismiss();
+        setTaskItems([...taskItems, task])
+        setTask(null);
+    }
+
+    const completeTask = (index) => {
+        let itemsCopy = [...taskItems];
+        itemsCopy.splice(index, 1);
+        setTaskItems(itemsCopy);
+    }
+
+
     return (
-        <View>
-            <Text>Diary Screen.</Text>
+        <View style={styles.container}>
+            <View style={styles.taskWrapper}>
+                <Text style={styles.sectionTitle}>Today's Tasks</Text>
+
+                <View style={styles.items}>
+                    {/* This is where the tasks will go */}
+                    {
+                        taskItems.map((item, index) => {
+                           return (
+                               <TouchableOpacity onPress={() => completeTask(index)}>
+                                    <Task text={item} />
+                               </TouchableOpacity>
+                           )
+                        })
+                    }
+                </View>
+            </View>
+
+            {/* Write a task */}
+            <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+                style={styles.writeTaskWrapper}  
+            >
+                <TextInput style={styles.input} placeholder={'write a task'} value={task} onChangeText={text => setTask(text)}/>
+
+                <TouchableOpacity onPress={() => handleAddTask()}>
+                    <View style={styles.addWrapper}>
+                        <Text style={styles.addText}>+</Text>
+                    </View>
+                </TouchableOpacity>
+            </KeyboardAvoidingView>
         </View>
     );
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    taskWrapper: {
+        paddingTop: 50,
+        paddingHorizontal: 20,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    items: {
+        marginTop: 30,
+    },
+    writeTaskWrapper: {
+        position: 'absolute',
+        bottom: 40,
+        width: "100%",
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+    input: {
+        paddingVertical: 15,
+        width: 250,
+        paddingHorizontal: 20,
+        backgroundColor: 'white',
+        borderRadius: 60,
+        borderColor: '#C0C0C0',
+        borderWidth: 1,
+
+    },
+    addWrapper: {
+        width: 60,
+        height: 60,
+        backgroundColor: 'white',
+        borderRadius: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: '#C0C0C0',
+        borderWidth: 1,
+    },
+    addText: {},
+});
 export default DiaryScreen;
