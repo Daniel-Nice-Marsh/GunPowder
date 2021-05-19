adaeaf42da5010ddce5f0c91b6dd876ec752e8ad
Daniel-Nice-Marsh <60605432+Daniel-Nice-Marsh@users.noreply.github.com>
2021-05-19 16:03:01 +0100

    Working login and register screen. firebase save image broken

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
 // import react components.
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';  
-import Task from './components/Task';
+import React, { Component } from 'react';  
+
+import {View, Text } from 'react-native';
+import * as firebase from 'firebase';
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCu0GBka8hGdP4dYeMhoOn3BEcSXtvJIfw",
+  authDomain: "gunpowder-3697e.firebaseapp.com",
+  projectId: "gunpowder-3697e",
+  storageBucket: "gunpowder-3697e.appspot.com",
+  messagingSenderId: "267419470524",
+  appId: "1:267419470524:web:b2dc078d5e0ae03ae6deb9",
+  measurementId: "G-NBJ5Q53TE9"
+};
+
+// initialise firebase 
+if(firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+} else {
+  firebase.app();
+}
 
 import { NavigationContainer } from '@react-navigation/native';  
 import { createStackNavigator } from '@react-navigation/stack';
-
-import { Dimensions, StyleSheet, SafeAreaView, Text, Image, TouchableOpacity, View, Button } from 'react-native'; 
-import styled from 'styled-components';
-
 
 // import screens.
 import WelcomeScreen from './screens/WelcomeScreen';
@@ -17,39 +33,71 @@ import DiaryScreen from './screens/DiaryScreen';
 import DiaryReviewScreen from './screens/DiaryReviewScreen';
 import PhotosScreen from './screens/PhotosScreen';
 import CameraScreen from './screens/CameraScreen';
+import LandingScreen from './components/LandingScreen';
+import RegisterScreen from './components/RegisterScreen';
+import LoginScreen from './components/LoginScreen';
 
 // create stack navigator.
 const Stack = createStackNavigator();
 
-// main.
-const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Welcome"
-          component={WelcomeScreen}
-        />
-        <Stack.Screen 
-          name="Diary" 
-          component={DiaryScreen} 
-        />
-        <Stack.Screen 
-          name="Diary Review" 
-          component={DiaryReviewScreen} 
-        />
-        <Stack.Screen 
-          name="Photos" 
-          component={PhotosScreen} 
-        />
-        <Stack.Screen 
-          name="Camera" 
-          component={CameraScreen} 
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+export class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      loaded: false,
+    }
+  }
+
+  componentDidMount(){
+    firebase.auth().onAuthStateChanged((user) => {
+      if(!user){
+        this.setState({
+          loggedIn: false,
+          loaded: true,
+        })
+      } else {
+        this.setState({
+          loggedIn: true,
+          loaded: true,
+        })
+      }
+    })
+  }
+  render() {
+    const { loggedIn, loaded } = this.state;
+    if(!loaded){
+      return(
+        <View style={{flex:1, justifyContent: 'center'}}>
+          <Text>Loading...</Text>
+        </View>
+      )
+    }
+    if (!loggedIn) {
+      return (
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Landing">
+            <Stack.Screen name="Landing" component={LandingScreen} options={{headerShown: false}} />
+            <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    } 
+    else {
+      return(
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Welcome" component={WelcomeScreen} />
+            <Stack.Screen name="Diary" component={DiaryScreen} />
+            <Stack.Screen name="Diary Review" component={DiaryReviewScreen} />
+            <Stack.Screen name="Photos" component={PhotosScreen} />
+            <Stack.Screen name="Camera" component={CameraScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+    }
+  }
+}
 
 export default App;
 
--- screens/PhotosScreen.js
+++ screens/PhotosScreen.js
@@ -1,10 +1,51 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { Text, View, TextInput, Image, Button } from 'react-native';
+
+import firebase from "firebase/app";
+require("firebase/firestore");
+require("firebase/firebase-storage");
 
 function PhotosScreen(props) {
+    const [caption, setCaption] = useState("");
+
+    const uploadImage = async () => {
+        const uri = props.route.params.image;
+        const childPath = `post/${firebase.auth().currentUser.uid}/${Math.random().toString(36)}`;
+        const response = await fetch(uri);
+        const blob = await response.blob();
+        
+
+        const task = firebase
+        .Storage()
+        .ref()
+        .child(childPath)
+        .put(blob);
+
+        {/* console log number of bytes transfered by snapshot */}
+        const taskProgress = snapshot => {
+            console.log(`transferred: ${snapshot.bytesTransferred}`)
+        }
+
+        const taskCompleted = () => {
+            task.snapshot.ref.getDownloadURL().then((snapshot) => {
+                console.log(snapshot);
+            })
+        }
+
+        const taskError = snapshot => {
+            console.log(snapshot);
+        }
+
+        task.on("state_changed", taskProgress, taskError, taskCompleted);
+    }
     return (
-        <View>
-            <Text>Photos Screen.</Text>
+        <View style={{ flex: 1 }}>
+            <Image source={{ uri: props.route.params.image }}/>
+            <TextInput 
+                placeholder="Write a caption..."
+                onChangeText={(caption) => setCaption(caption)}
+            />
+            <Button title="Save" onPress={() => uploadImage()}/>
         </View>
     );
 }
